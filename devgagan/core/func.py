95501b0793d31d3abe807cf855e847b63c3ee759
Sushilgithubbot <sushilgithubbot@gmail.com>
2024-08-13 14:17:00 +0530

    Update func.py

--- devgagan/core/func.py
+++ devgagan/core/func.py
@@ -89,7 +89,7 @@ PROGRESS_BAR = """\n
 **__Bytes__** : {0}%
 **__Speed__** : {3}/s
 **__Time__** : {4}\n\n
-**__Powered by [Team SPY](https://t.me/devggn)__**
+**__Powered by [Team SK](https://t.me/advancedchats)__**
 """
 
 
